fbe3fa63133364f341d30196adb2e7d1c494d3ce
John Newman <newmanj903@gmail.com>
2018-08-10 11:36:56 +0100

    heal created

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 new Vue({
     el: '#app',
     data: {
-        start: false
-    }
-    
+        start: false,
+        myHealth: 100,
+        monsterHealth: 100
+    },
+    methods: {
+        attack() {
+            this.monsterHealth -= Math.random()*20;
+            if(this.monsterHealth <= 0) {this.monsterHealth = 0}
+            this.monsterAttack();
+        },
+        monsterAttack() {
+            this.myHealth -= Math.random()*20;
+            if(this.myHealth <= 0) {this.myHealth = 0}
+        },
+        heal() {
+            this.myHealth += Math.random()*20 + 5;
+            if(this.myHealth >= 100) {this.myHealth = 100}
+            this.monsterAttack();
 
+        }
+    }
 })
